1a82ce607a3b7cc2420b3a5efa5294068a77bb45
Anindow <anindowsaikat@gmail.com>
2023-01-06 16:53:52 +0600

    seasons finished

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,38 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    // this is the only time we do direct assignment to this.state
-    this.state = { lat: null, long: null, errorMessage: '' };
-    // this.state = { long: null };
+  state = { lat: null, long: null, errorMessage: "" };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        //we called setstate || we did not !! this.state.lat = position coords.latitude;
+      (position) =>
         this.setState({
           lat: position.coords.latitude,
           long: position.coords.longitude,
-        });
-        // this.setState({ lat: position.coords.latitude ,});
-        // this.setState({ long: position.coords.longitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+        }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error : {this.state.errorMessage}</div>;
     }
-    return (
-      <div>
-        Latitude: {this.state.lat} <br />
-        Longitude: {this.state.long}
-      </div>
-    )
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <Spinner message="PLease accept location request" />;
   }
+  render() {
+    return <div className="border red"> {this.renderContent()}</div>;
+  
 }
-
+}
 ReactDOM.createRoot(document.querySelector("#root")).render(<App />);
