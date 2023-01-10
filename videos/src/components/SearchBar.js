926557919a388c6c6bae8f3d41517ad9ec1e98d1
Anindow <anindowsaikat@gmail.com>
2023-01-10 14:52:06 +0600

    just a inline css for upper margine

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -11,8 +11,11 @@ class SearchBar extends React.Component {
   };
   render() {
     return (
-      <div className="search-bar ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
+      <div className="search-bar ui segment" >
+        <form
+          onSubmit={this.onFormSubmit}
+          className="ui form"
+          >
           <div className="field">
             <label>Video Search</label>
             <input
