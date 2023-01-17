5579e6e623c4928df9080c5e2fa570e6b6d55a42
Anindow <anindowsaikat@gmail.com>
2023-01-17 13:35:08 +0600

    update daily

--- widgets/src/component/App.js
+++ widgets/src/component/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from "./Accordion";
 import Search from './Search';
-
+import Dropdown from './Dropdown';
+import Translate from './Translate';
+import Route from './Route';
+import Header from './Header';
 const items =[
     {
         title: 'What is React?',
@@ -16,13 +19,49 @@ const items =[
         content: 'You use React by creating components'
     }
 ];
+const options =[
+    {
+        label : 'Red',
+        value : "red"
+    },
+    {
+        label : 'Blue',
+        value : "blue"
+    },
+    {
+        label : 'Green',
+        value : "green"
+    }
+];
 
 const App = () => {
-  return (
-    <div>
-      <Search />
-    </div>
-  );
+const[selected,setSelected]=useState(options[0]);
+ 
+ 
+ 
+    return (
+      <div>
+        <Header />
+        <Route path="/" >
+            <Accordion items={items} />
+            </Route>
+        <Route path="/list" >
+            <Search items={items} />
+            </Route>
+        <Route path="/translate" >
+            <Translate items={items} />
+            </Route>
+        <Route path="/dropdown" >
+            <Dropdown items={items} 
+            label="Select a Color"
+            options={options}
+            selected={selected}
+            onSelectedChange={setSelected}
+            />
+            </Route>
+
+      </div>
+    );
 };
 export default App;
 
--- widgets/src/component/Search.js
+++ widgets/src/component/Search.js
@@ -1,39 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState('programming');
+  const [term, setTerm] = useState("programming");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
   useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
+
+  useEffect(() => {
     const search = async () => {
-      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
-          action: 'query',
-          list: 'search',
-          origin: '*',
-          format: 'json',
-          srsearch: term,
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: debouncedTerm,
         },
       });
-
       setResults(data.query.search);
     };
-
-    if (term && !results.length) {
-      search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 1000);
-
-      return () => {
-        clearTimeout(timeoutId);
-      };
-    }
-  }, [term]);
+    search();
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => {
     return (
@@ -41,8 +37,7 @@ const Search = () => {
         <div className="right floated content">
           <a
             className="ui button"
-            href={`https://en.wikipedia.org?curid=${result.pageid}`}
-          >
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}>
             Go
           </a>
         </div>
