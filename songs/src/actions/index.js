c261893f60e69a3aa65eb0c927499a54c4ddf06b
Anindow <anindowsaikat@gmail.com>
2023-01-31 21:45:26 +0600

    blog is just an empty project only sons got polished in this commit

--- songs/src/actions/index.js
+++ songs/src/actions/index.js
@@ -1,6 +1,8 @@
- export const selectSong = () => {
-    return{
-        type : 'SONG_SELECTED',
-        payload: song
-    };
-}
+// Action creator
+export const selectSong = song => {
+  // Return an action
+  return {
+    type: "SONG_SELECTED",
+    payload: song
+  };
+};
--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,8 +1,38 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React,{Component} from 'react';
+import { connect } from 'react-redux';
+import { selectSong } from "../actions";
 class SongList extends Component {
-rendar() {
-return <div>SongList</div>
+    renderList() {
+        return this.props.songs.map(song => {
+            return (
+              <div className="item" key={song.title}>
+                <div className="right floated content">
+                  <button
+                    onClick={() => this.props.selectSong(song)}
+                    className="ui button primary">
+                    Select
+                  </button>
+                </div>
+                <div className="content">
+                  {song.title}
+                  <div>
+                    <b>
+                      {song.title === this.props.favoriteTitle && "Favorite!"}
+                    </b>
+                  </div>
+                </div>
+              </div>
+            );
+        });
+    }
+
+    render() {
+        return <div className="ui divided list">{this.renderList()}</div>;
+    }
 }
-}
-export default connect()(SongList);
+
+const mapStateToProps = state => {
+    return { songs: state.songs, favoriteTitle: state.favoriteTitle };
+};
+
+export default connect(mapStateToProps, { selectSong })(SongList);
