46f6da88d8613d9367f88dba3c7d323272c632bb
Anindow <anindowsaikat@gmail.com>
2023-02-07 03:55:02 +0600

    blog done

--- blog/src/index.js
+++ blog/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from "./components/App";
 import reducers from "./reducers";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore,applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+
+const store = createStore(reducers,applyMiddleware(thunk));
 
 ReactDOM.createRoot(document.querySelector("#root")).render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>
 );
